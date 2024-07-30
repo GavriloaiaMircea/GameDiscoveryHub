694da2540c756e6b04128c129de498718539c7ae
GavriloaiaMircea <rowan.mtg@yahoo.com>
2024-07-30 13:47:03 +0300

    chore: Add react-icons package and update GameCard component

--- src/Components/GameCard.tsx
+++ src/Components/GameCard.tsx
@@ -1,17 +1,25 @@
-import { Card ,CardBody,CardHeader,Heading,Image} from "@chakra-ui/react"
-import { Game } from "../hooks/useGames"
-interface Props{
-    game: Game
+import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
+import { Game } from "../hooks/useGames";
+import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
+interface Props {
+  game: Game;
 }
-const GameCard = ({game} : Props) => {
+const GameCard = ({ game }: Props) => {
   return (
     <Card borderRadius={10} overflow="hidden">
-        <Image src={game.background_image}/>
-        <CardBody>
-            <Heading fontSize='2xl'>{game.name}</Heading>
-        </CardBody>
+      <Image src={game.background_image} />
+      <CardBody>
+        <Heading fontSize="2xl">{game.name}</Heading>
+        <HStack justifyContent={"space-between"}>
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
+      </CardBody>
     </Card>
-  )
-}
+  );
+};
 
-export default GameCard
+export default GameCard;
