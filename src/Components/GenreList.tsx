646ce9363c388fe287c193660a25c0b10e555569
GavriloaiaMircea <rowan.mtg@yahoo.com>
2024-07-30 16:56:41 +0300

    Genre List now works and sorts games based on the selected genre

--- src/Components/GenreList.tsx
+++ src/Components/GenreList.tsx
@@ -1,22 +1,39 @@
-import { HStack, List, ListItem ,Image,Text,Spinner} from "@chakra-ui/react";
+import { HStack, List, ListItem, Image, Button } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCropedImage from "../services/image-url";
 import GenreListSkeleton from "./GenreListSkeleton";
+import { Genre } from "../hooks/useGenres";
 
-const GenreList = () => {
-  const { data , isLoading, err} = useGenres();
-  const skeleton = [1, 2, 3, 4, 5, 6, 7, 8,9,10,11,12];
+interface Props {
+  onSelectGenre: (genre: Genre) => void;
+}
 
-  if(err) return null;
+const GenreList = ({ onSelectGenre }: Props) => {
+  const { data, isLoading, err } = useGenres();
+  const skeleton = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+  if (err) return null;
 
   return (
     <List>
-        {isLoading && skeleton.map((i)=><GenreListSkeleton key={i}/>)}
+      {isLoading && skeleton.map((i) => <GenreListSkeleton key={i} />)}
       {data.map((genre) => (
         <ListItem key={genre.id} paddingY="5px">
           <HStack>
-            <Image boxSize="32px" borderRadius={8} src={getCropedImage(genre.image_background)} />
-            <Text fontSize="lg">{genre.name}</Text>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCropedImage(genre.image_background)}
+            />
+            <Button
+              variant="link"
+              onClick={() => onSelectGenre(genre)}
+              fontSize="md"
+              whiteSpace="pre-wrap"
+              textAlign="left"
+            >
+              {genre.name}
+            </Button>
           </HStack>
         </ListItem>
       ))}
