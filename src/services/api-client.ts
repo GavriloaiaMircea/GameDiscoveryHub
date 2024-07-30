4b1b6350bed6f09a193b3a24048c15dd25e14b05
GavriloaiaMircea <rowan.mtg@yahoo.com>
2024-07-30 16:56:41 +0300

    Genre List now works and shorts the games based on the selected genre

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "fc937c05fefe4ebeb1b799024741640f",
+    key: " YOUR_API_KEY",
   },
 });
