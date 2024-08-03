d4c16d1653841d5804bdb47247eb871292d80f04
GavriloaiaMircea <rowan.mtg@yahoo.com>
2024-08-03 16:07:54 +0300

    chore: Fix error handling in GameGrid component and update theme colors

--- src/Components/GameGrid.tsx
+++ src/Components/GameGrid.tsx
@@ -12,13 +12,15 @@ interface Props {
 const GameGrid = ({ gameQuerry }: Props) => {
   const { data, err, isLoading } = useGames(gameQuerry);
   const skeleton = [1, 2, 3, 4, 5, 6, 7, 8];
+
+  if(err) return <Text color="tomato">{err}</Text>
+
   return (
     <SimpleGrid
       padding="10px"
       columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
       spacing={6}
     >
-      {err && <Text color="tomato">{err}</Text>}
       {data.map((game) => (
         <GameCardContainer key={game.id}>
           <GameCard game={game} />
