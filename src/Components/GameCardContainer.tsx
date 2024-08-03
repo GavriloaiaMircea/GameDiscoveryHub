eb03ce9f1bf8b3b38fb9cd7fc0ab063bf36e4fb3
GavriloaiaMircea <rowan.mtg@yahoo.com>
2024-08-03 15:59:30 +0300

    feat: Add rating display to GameCard component

--- src/Components/GameCardContainer.tsx
+++ src/Components/GameCardContainer.tsx
@@ -6,7 +6,7 @@ interface Props{
 }
 const GameCardContainer = ({children} : Props) => {
   return (
-    <Box height="350" borderRadius={10} overflow="hidden">{children}</Box>
+    <Box borderRadius={10} overflow="hidden">{children}</Box>
   )
 }
 
--- src/Components/GenreList.tsx
+++ src/Components/GenreList.tsx
@@ -8,7 +8,6 @@ import {
 } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCropedImage from "../services/image-url";
-import GenreListSkeleton from "./GenreListSkeleton";
 import { Genre } from "../hooks/useGenres";
 interface Props {
   onSelectGenre: (genre: Genre) => void;
@@ -16,11 +15,7 @@ interface Props {
 }
 
 const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
-  const { data, isLoading, err } = useGenres();
-  const skeleton = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-
-  if (err) return null;
-  if (err) return null;
+  const { data } = useGenres();
 
   return (
     <>
@@ -28,8 +23,6 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
         Genres
       </Heading>
       <List>
-        {isLoading && skeleton.map((i) => <GenreListSkeleton key={i} />)}
-        {isLoading && skeleton.map((i) => <GenreListSkeleton key={i} />)}
         {data.map((genre) => (
           <ListItem key={genre.id} paddingY="5px">
             <HStack>
