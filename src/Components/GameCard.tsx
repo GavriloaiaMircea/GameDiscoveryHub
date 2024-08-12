209786630f1cd85dc9e1f828e503abb01d1629a7
GavriloaiaMircea <rowan.mtg@yahoo.com>
2024-08-12 23:07:03 +0300

    Refactoring entities

--- src/Components/GameCard.tsx
+++ src/Components/GameCard.tsx
@@ -1,5 +1,5 @@
 import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
-import { Game } from "../hooks/useGames";
+import { Game } from "../entities/Game";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
@@ -11,7 +11,7 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card>
-      <Image src={getCroppedImageUrl (game.background_image)} />
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <HStack justifyContent={"space-between"} marginBottom={3}>
           <PlatformIconList
@@ -19,8 +19,10 @@ const GameCard = ({ game }: Props) => {
           />
           <CriticScore score={game.metacritic} />
         </HStack>
-        <Heading fontSize="2xl"><Link to={`/games/${game.slug}`}>{game.name}</Link></Heading>
-        <Emoji rating={game.rating_top}/>
+        <Heading fontSize="2xl">
+          <Link to={`/games/${game.slug}`}>{game.name}</Link>
+        </Heading>
+        <Emoji rating={game.rating_top} />
       </CardBody>
     </Card>
   );
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,20 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client";
 import genres from "../data/genres";
+import { Genre } from "../entities/Genre";
 
 const apiClient = new APIClient<Genre>("/genres");
-export interface Genre {
-  id: number;
-  name: string;
-  image_background: string;
-}
-
 const useGenres = () =>
   useQuery({
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, // 24 hours
-    initialData:genres
+    initialData: genres,
   });
 
 export default useGenres;
